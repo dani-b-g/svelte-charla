cd1f86b430f273a7cfb22a17c973541f451bf9ce
Daniel Barriga Grados <dbarriga@viewnext.com>
2022-11-02 14:50:56 +0100

    añadida tabla preguntas

--- src/lib/Views/Preguntas.svelte
+++ src/lib/Views/Preguntas.svelte
@@ -15,6 +15,7 @@
 
   let formEstado = undefined;
   let formPregunta = undefined;
+  let preguntas = [];
 
   onMount(() => {});
 
@@ -37,10 +38,56 @@
       sw.fire("Error al crear la pregunta", error, "error");
     }
   }
+  onSnapshot(
+    collection(db, "preguntas"),
+    (querySnapshot) => {
+      preguntas = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
 </script>
 
-<h1>Preguntas</h1>
+<div class="row valign-wrapper">
+  <div class="col s12 center-align">
+    <h1>Preguntas</h1>
+  </div>
+</div>
+<div class="row">
+  <div class="col offset-s1 s10 center-align offset-s1">
+    <table class="responsive-table centered striped">
+      <thead>
+        <tr>
+            <th>Pregunta</th>
+            <th>Estado</th>
+            <th>Botones</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each preguntas as p}
+        <tr>
+          <td>{p.Pregunta}</td>
+          <td>{p.Estado}</td>
+          <td>
+            <button class="btn waves-effect waves-light green lighten-1">Submit
+              <i class="material-icons right">check</i>
+            </button>
+          </td>
+        </tr>
+        {:else}
+           <!-- empty list -->
+        {/each}
+      </tbody>
+    </table>
+  </div>
 
+</div>
 <style>
   /* your styles go here */
+  /* para fondo de tabla */
+  /* rgba(5,172,232,0.4514180672268907) */
 </style>
