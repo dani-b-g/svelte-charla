342c624c4ad770c0758ed31349875d78ba52d1a6
Daniel Barriga Grados <dbarriga@viewnext.com>
2022-11-03 14:36:29 +0100

    Para mostrar codigo formateado

---
+++ src/lib/Components/CodeBlock.svelte
@@ -0,0 +1,30 @@
+<script>
+    
+    import Prism from 'prismjs';
+    // import 'prismjs/themes/prism-tomorrow.css'
+    // import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
+    // import 'prismjs/plugins/line-numbers/prism-line-numbers.js'
+    // import 'prismjs/themes/'
+    import 'prismjs/plugins/line-numbers/prism-line-numbers.js'
+    import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
+    import 'prismjs/themes/prism-tomorrow.css'
+
+
+
+    
+    export let code = undefined;
+</script>
+
+
+<div>
+    <pre class=" line-numbers language-js ">
+        <code >
+            {@html Prism.highlight(code, Prism.languages.javascript,'javascript')}
+        </code>
+    </pre>
+</div>
+
+
+<style>
+   
+</style>
